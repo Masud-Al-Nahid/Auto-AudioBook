cb9cb581c3edda63440262e05277dd594949ef81
Aaron Marriner <amarriner@amarriner.com>
2014-02-26 15:42:16 +0000

    Add logic to ensure we don't attempt to go past end of book

--- auto_audiobook.py
+++ auto_audiobook.py
@@ -21,23 +21,26 @@ f.close()
 
 # Loop through the book until we find enough non-blank lines, accumulating them in a list
 for i in range(int(Config.get('Auto', 'Last Line')), int(Config.get('Auto', 'Last Line')) + 500):
-   if (len(book[i].strip())):
-      text_lines  = text_lines + 1
-   else:
-      blank_lines = blank_lines + 1
-      if text_lines >= int(Config.get('Book', 'Lines')):
-         break
+   if (i < len(book) - 1):
+      if (len(book[i].strip())):
+         text_lines  = text_lines + 1
+      else:
+         blank_lines = blank_lines + 1
+         if text_lines >= int(Config.get('Book', 'Lines')):
+            break
 
-   audio_text.append(book[i])
+      audio_text.append(book[i])
 
 # Update the config file with the line we stopped at
 f = open(INI_FILE, 'w')
 Config.set('Auto', 'Last Line', int(Config.get('Auto', 'Last Line')) + len(audio_text))
-Config.set('Youtube', 'Episode', int(Config.get('Youtube', 'Episode')) + 1)
+next_episode = int(Config.get('Youtube', 'Episode')) + 1
+Config.set('Youtube', 'Episode', next_episode)
 Config.write(f)
 
 # Write the text to be converted out to a file
 f = open('out.txt', 'w')
+f.write('Episode number ' + str(next_episode) + '\n\n')
 for line in audio_text:
    f.write(line.strip() + '\n')
 f.close()
@@ -51,7 +54,7 @@ print 'Total Length: ' + str(len(audio_text))
 subprocess.call('./make_audio.shl')
 
 # Call the shell script that uses the book image and espeack WAV create above to generate an MP4 file
-subprocess.call('./make_video.shl')
+subprocess.call(['./make_audio.shl', Config.get('Auto', 'Speed')])
 
 # Upload the file to Youtube and place it in a playlist
 subprocess.call(['./upload.py', '--noauth_local_webserver', '--file', 'out.mp4'])
